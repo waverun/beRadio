20cbafd3e6ef27a35b37c069e1169e3f46e70035
Shay Moreno <waverun@me.com>
2023-04-04 22:07:40 +0300

    play background

--- beRadio/Observables/AudioPlayer.swift
+++ beRadio/Observables/AudioPlayer.swift
@@ -1,3 +1,55 @@
+//import SwiftUI
+//import AVFoundation
+//
+//class AudioPlayer: ObservableObject {
+//    private var player: AVPlayer?
+//    @Published var isPlaying = false
+//
+//    @Published var currentProgressString: String = "00:00"
+//    @Published var totalDurationString: String = "00:00"
+//
+//    let timeFormatter: DateComponentsFormatter = {
+//        let formatter = DateComponentsFormatter()
+//        formatter.allowedUnits = [.hour, .minute, .second]
+//        formatter.zeroFormattingBehavior = [.dropLeading, .pad]
+//        return formatter
+//    }()
+//
+//    private var timeObserverToken: Any?
+//
+////    func play(url: URL) {
+////        player = AVPlayer(url: url)
+////        player?.play()
+////        isPlaying = true
+////        Task {
+////            await updateTotalDurationString()
+////        }
+////        startUpdatingCurrentProgress()
+////    }
+//
+//    func play(url: URL) {
+//        if player == nil {
+//            if let timeObserverToken = timeObserverToken {
+//                player?.removeTimeObserver(timeObserverToken)
+//            }
+//
+//            player = AVPlayer(url: url)
+//            Task {
+//                await updateTotalDurationString()
+//            }
+//            startUpdatingCurrentProgress()
+//        }
+//
+//        player?.play()
+//        isPlaying = true
+//    }
+//
+//    func pause() {
+//        player?.pause()
+//        isPlaying = false
+//        stopUpdatingCurrentProgress()
+//    }
+
 import SwiftUI
 import AVFoundation
 
@@ -7,14 +59,34 @@ class AudioPlayer: ObservableObject {
 
     @Published var currentProgressString: String = "00:00"
     @Published var totalDurationString: String = "00:00"
-
-//    let timeFormatter: DateComponentsFormatter = {
-//        let formatter = DateComponentsFormatter()
-//        formatter.allowedUnits = [.minute, .second]
-//        formatter.zeroFormattingBehavior = .pad
-//        return formatter
-//    }()
     
+    init() {
+//        super.init()
+        configureAudioSession()
+    }
+
+    private func configureAudioSession() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playback, mode: .default, options: [.allowAirPlay])
+            try audioSession.setActive(true, options: [])
+        } catch {
+            print("Failed to set up audio session: \(error)")
+        }
+    }
+
+//    // ...
+//
+//    private func configureAudioSession() {
+//        let audioSession = AVAudioSession.sharedInstance()
+//        do {
+//            try audioSession.setCategory(.playback, mode: .default, options: [])
+//            try audioSession.setActive(true, options: [])
+//        } catch {
+//            print("Failed to set up audio session: \(error)")
+//        }
+//    }
+
     let timeFormatter: DateComponentsFormatter = {
         let formatter = DateComponentsFormatter()
         formatter.allowedUnits = [.hour, .minute, .second]
@@ -23,15 +95,22 @@ class AudioPlayer: ObservableObject {
     }()
 
     private var timeObserverToken: Any?
+    private var isObservingTime = false
 
     func play(url: URL) {
-        player = AVPlayer(url: url)
+        if player == nil {
+            player = AVPlayer(url: url)
+            Task {
+                await updateTotalDurationString()
+            }
+        }
+
+        if !isObservingTime {
+            startUpdatingCurrentProgress()
+        }
+
         player?.play()
         isPlaying = true
-        Task {
-            await updateTotalDurationString()
-        }
-        startUpdatingCurrentProgress()
     }
 
     func pause() {
@@ -39,6 +118,9 @@ class AudioPlayer: ObservableObject {
         isPlaying = false
         stopUpdatingCurrentProgress()
     }
+
+    
+    // ... rest of the class code
 
     func forward() {
         if let player = player {
@@ -70,11 +152,6 @@ class AudioPlayer: ObservableObject {
         }
     }
 
-//    private func updateTotalDurationString() async {
-//        if let currentItem = player?.currentItem {
-//            let duration = await currentItem.asset.load(.duration)
-//            totalDurationString = timeFormatter.string(from: duration.seconds) ?? "00:00"
-//        }
     private func updateTotalDurationString() async {
         if let currentItem = player?.currentItem {
             do {
@@ -88,38 +165,3 @@ class AudioPlayer: ObservableObject {
         }
     }
 }
-
-//import SwiftUI
-//import AVFoundation
-//
-//class AudioPlayer: ObservableObject {
-//    private var player: AVPlayer?
-//    @Published var isPlaying = false
-//
-//    func play(url: URL) {
-//        player = AVPlayer(url: url)
-//        player?.play()
-//        isPlaying = true
-//    }
-//
-//    func pause() {
-//        player?.pause()
-//        isPlaying = false
-//    }
-//
-//    func forward() {
-//        if let player = player {
-//            let forwardTime = CMTimeMake(value: 15, timescale: 1)
-//            let newTime = CMTimeAdd(player.currentTime(), forwardTime)
-//            player.seek(to: newTime)
-//        }
-//    }
-//
-//    func rewind() {
-//        if let player = player {
-//            let rewindTime = CMTimeMake(value: -15, timescale: 1)
-//            let newTime = CMTimeAdd(player.currentTime(), rewindTime)
-//            player.seek(to: newTime)
-//        }
-//    }
-//}
