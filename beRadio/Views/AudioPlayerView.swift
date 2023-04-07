b3fcb26a91cb43af3461d0f3fc73977a7c5ce97e
Shay Moreno <waverun@me.com>
2023-04-07 17:54:50 +0300

    add audio switcher button

--- beRadio/Views/AudioPlayerView.swift
+++ beRadio/Views/AudioPlayerView.swift
@@ -18,53 +18,73 @@ struct AudioPlayerView: View {
     }
     
     var body: some View {
-        VStack {
-            Text(heading)
-            AsyncImage(url: "https://103fm.maariv.co.il" + imageSrc)
-                .frame(width: 240, height: 240) // Adjust the size as needed
+        GeometryReader { geometry in
             HStack {
-                Text("\(audioPlayer.totalDurationString)")
+                Spacer()
+                VStack {
+                    VStack {
+                        Text(heading)
+                        AsyncImage(url: "https://103fm.maariv.co.il" + imageSrc)
+                            .frame(width: 240, height: 240)
+                        
+                        HStack {
+                            Text("\(audioPlayer.totalDurationString)")
+                                .foregroundColor(.white)
+                            
+                            Button(action: {
+                                audioPlayer.rewind()
+                            }) {
+                                Image(systemName: "gobackward.15")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40)
+                            }
+                            
+                            Button(action: {
+                                audioPlayer.isPlaying ? audioPlayer.pause() : audioPlayer.play(url: audioUrl)
+                            }) {
+                                Image(systemName: audioPlayer.isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 50, height: 50)
+                            }
+                            
+                            Button(action: {
+                                audioPlayer.forward()
+                            }) {
+                                Image(systemName: "goforward.15")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40)
+                            }
+                            
+                            Text("\(audioPlayer.currentProgressString)")
+                                .foregroundColor(.white)
+                        }
+                    }
+                    
+                    let availableSpace = geometry.size.height - geometry.safeAreaInsets.bottom - geometry.size.width / 2
+                    Spacer()
+                        .frame(height: availableSpace / 2)
+                    
+                    Button(action: {
+                        selectAudioOutput()
+                    }) {
+                        Image(systemName: "airplayaudio")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 24, height: 24)
+                    }
+                    .padding()
+                    .background(Color.blue)
                     .foregroundColor(.white)
-                
-                Button(action: {
-                    audioPlayer.rewind()
-                }) {
-                    Image(systemName: "gobackward.15")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 40, height: 40)
+                    .cornerRadius(8)
+                    
+                    Spacer()
                 }
-                
-                Button(action: {
-                    audioPlayer.isPlaying ? audioPlayer.pause() : audioPlayer.play(url: audioUrl)
-                }) {
-                    Image(systemName: audioPlayer.isPlaying ? "pause.circle.fill" : "play.circle.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 50, height: 50)
-                }
-                
-                Button(action: {
-                    audioPlayer.forward()
-                }) {
-                    Image(systemName: "goforward.15")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 40, height: 40)
-                }
-                
-                Text("\(audioPlayer.currentProgressString)")
-                    .foregroundColor(.white)
+                Spacer()
             }
-            Button("Select Audio Output") {
-                selectAudioOutput()
-            }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(8)
+            .edgesIgnoringSafeArea(.bottom)
         }
-        
     }
 }
-
