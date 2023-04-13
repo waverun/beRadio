0f9bfe959f06b3a1fd502fda859808d0ccf4f184
Shay Moreno <waverun@me.com>
2023-04-13 21:38:45 +0300

    add radio stations

--- beRadio/ContentView.swift
+++ beRadio/ContentView.swift
@@ -14,15 +14,15 @@ struct ContentView: View {
     
     @State private var title = "beRadio"
     @State private var showingAddLinkView = false
-//    @State private var showingSearchIcon = true
+    //    @State private var showingSearchIcon = true
     @State private var showLivePlayerView: Bool = false
     @State private var showingRadioStationsView = false
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-        
+    
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Link.url, ascending: true)],
                   animation: .default) private var links: FetchedResults<Link>
     
@@ -32,94 +32,100 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             NavigationView {
-//                List {
-//                    ForEach(items) { item in
-//                        NavigationLink {
-//                            if links.isEmpty {
-//                                Text("Loading...")
-//                            } else {
-//                                List {
-//                                    HStack {
-//                                        Spacer()
-//                                        Button(action: {
-//                                            showLivePlayerView.toggle()
-//                                        }) {
-//                                            Text("Live!")
-//                                                .padding(.horizontal, 8) // Adjust horizontal padding
-//                                                .padding(.vertical, 8) // Adjust vertical padding
-//                                                .background(Color.blue)
-//                                                .foregroundColor(.white)
-//                                                .cornerRadius(8)
-//                                        }
-//                                        Spacer()
-//                                    }
-//                                    ForEach(links, id: \.self) { link in
-//                                        if !removedLinks.contains(where: { $0.url == link.url }) {
-//                                            NavigationLink(destination: fullProgramsView(link: link.url!)) {
-//                                                let text = link.url!.replacingOccurrences(of: "/program/", with: "").replacingOccurrences(of: ".aspx", with: "")
-//
-//                                                LinkButton(label: text, link: link.url!) { _ in }
-//                                            }
-//                                        }
-//                                    }
-//                                    .onDelete(perform: removeLinks)
-//                                    .padding(.top, -10)
-//                                    .padding(.bottom, -10)
-//                                }
-//                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-//                                .onAppear {
-//                                    title = "103 FM"
-//                                }
-//                                .sheet(isPresented: $showLivePlayerView) {
-//                                    AudioPlayerView(url: URL(string: "https://cdn.cybercdn.live/103FM/Live/icecast.audio")!, image: nil, date: "103 FM")
-//                                }
-//                                .toolbar {
-//#if os(iOS)
-//                                    ToolbarItem(placement: .navigationBarTrailing) {
-//                                        EditButton()
-//                                    }
-//#endif
-//                                    ToolbarItem {
-//                                        //                                    Button(action: addNewLink) {
-//                                        //                                        Label("Add link", systemImage: "plus")
-//                                        //                                    }
-//                                        Button(action: { showingAddLinkView.toggle() }) {
-//                                            Label("Add link", systemImage: "plus")
-//                                        }
-//                                    }
-//                                }
-//                                .sheet(isPresented: $showingAddLinkView) {
-//                                    //                                AddLinkView(links: $links)
-//                                    AddLinkView(links: .constant(Array(links)), removedLinks: .constant(Array(removedLinks)))
-//                                }
-//                            }
-//                            //                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-//                            Text("beRadio")
-//                        } label: {
-//                            Text(item.name ?? "New station")
-//                        }
-//                    }
-//                    .onDelete(perform: deleteItems)
-//                }
+                //                List {
+                //                    ForEach(items) { item in
+                //                        NavigationLink {
+                //                            if links.isEmpty {
+                //                                Text("Loading...")
+                //                            } else {
+                //                                List {
+                //                                    HStack {
+                //                                        Spacer()
+                //                                        Button(action: {
+                //                                            showLivePlayerView.toggle()
+                //                                        }) {
+                //                                            Text("Live!")
+                //                                                .padding(.horizontal, 8) // Adjust horizontal padding
+                //                                                .padding(.vertical, 8) // Adjust vertical padding
+                //                                                .background(Color.blue)
+                //                                                .foregroundColor(.white)
+                //                                                .cornerRadius(8)
+                //                                        }
+                //                                        Spacer()
+                //                                    }
+                //                                    ForEach(links, id: \.self) { link in
+                //                                        if !removedLinks.contains(where: { $0.url == link.url }) {
+                //                                            NavigationLink(destination: fullProgramsView(link: link.url!)) {
+                //                                                let text = link.url!.replacingOccurrences(of: "/program/", with: "").replacingOccurrences(of: ".aspx", with: "")
+                //
+                //                                                LinkButton(label: text, link: link.url!) { _ in }
+                //                                            }
+                //                                        }
+                //                                    }
+                //                                    .onDelete(perform: removeLinks)
+                //                                    .padding(.top, -10)
+                //                                    .padding(.bottom, -10)
+                //                                }
+                //                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                //                                .onAppear {
+                //                                    title = "103 FM"
+                //                                }
+                //                                .sheet(isPresented: $showLivePlayerView) {
+                //                                    AudioPlayerView(url: URL(string: "https://cdn.cybercdn.live/103FM/Live/icecast.audio")!, image: nil, date: "103 FM")
+                //                                }
+                //                                .toolbar {
+                //#if os(iOS)
+                //                                    ToolbarItem(placement: .navigationBarTrailing) {
+                //                                        EditButton()
+                //                                    }
+                //#endif
+                //                                    ToolbarItem {
+                //                                        //                                    Button(action: addNewLink) {
+                //                                        //                                        Label("Add link", systemImage: "plus")
+                //                                        //                                    }
+                //                                        Button(action: { showingAddLinkView.toggle() }) {
+                //                                            Label("Add link", systemImage: "plus")
+                //                                        }
+                //                                    }
+                //                                }
+                //                                .sheet(isPresented: $showingAddLinkView) {
+                //                                    //                                AddLinkView(links: $links)
+                //                                    AddLinkView(links: .constant(Array(links)), removedLinks: .constant(Array(removedLinks)))
+                //                                }
+                //                            }
+                //                            //                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                //                            Text("beRadio")
+                //                        } label: {
+                //                            Text(item.name ?? "New station")
+                //                        }
+                //                    }
+                //                    .onDelete(perform: deleteItems)
+                //                }
                 List {
-                        ForEach(items) { item in
+                    ForEach(items) { item in
+                        HStack {
+                            if let url = item.favicon {
+                                AsyncImage(url: url)
+                                    .frame(width: 60, height: 60) // Adjust the size as needed
+                            }
                             NavigationLink {
                                 if links.isEmpty {
                                     Text("Loading...")
                                 } else {
                                     ProgramsListView(links: links,
-                                                  removedLinks: removedLinks,
-                                                  removeLinks: removeLinks(atOffsets:),
-                                                  title: $title,
-                                                  showLivePlayerView: $showLivePlayerView,
-                                                  showingAddLinkView: $showingAddLinkView)
+                                                     removedLinks: removedLinks,
+                                                     removeLinks: removeLinks(atOffsets:),
+                                                     title: $title,
+                                                     showLivePlayerView: $showLivePlayerView,
+                                                     showingAddLinkView: $showingAddLinkView)
                                 }
                             } label: {
                                 Text(item.name ?? "New station")
                             }
                         }
-                        .onDelete(perform: deleteItems)
                     }
+                    .onDelete(perform: deleteItems)
+                }
                 .sheet(isPresented: $showLivePlayerView) {
                     AudioPlayerView(url: URL(string: "https://cdn.cybercdn.live/103FM/Live/icecast.audio")!, image: nil, date: "103 FM")
                 }
@@ -151,27 +157,27 @@ struct ContentView: View {
                 .navigationBarTitle(title, displayMode: .inline)
                 Text("Select an item")
             }
-//            if showingSearchIcon {
-//                VStack {
-//                    Spacer()
-//                    HStack {
-//                        Spacer()
-//                        Button(action: {
-//                            showingRadioStationsView.toggle()
-//                        }) {
-//                            Image(systemName: "magnifyingglass")
-//                                .resizable()
-//                                .frame(width: 16, height: 16)
-//                                .padding(10)
-//                                .background(Color.blue)
-//                                .foregroundColor(.white)
-//                                .clipShape(Circle())
-//                                .padding(.trailing, 16)
-//                                .padding(.bottom, 16)
-//                        }
-//                    }
-//                }
-//            }
+            //            if showingSearchIcon {
+            //                VStack {
+            //                    Spacer()
+            //                    HStack {
+            //                        Spacer()
+            //                        Button(action: {
+            //                            showingRadioStationsView.toggle()
+            //                        }) {
+            //                            Image(systemName: "magnifyingglass")
+            //                                .resizable()
+            //                                .frame(width: 16, height: 16)
+            //                                .padding(10)
+            //                                .background(Color.blue)
+            //                                .foregroundColor(.white)
+            //                                .clipShape(Circle())
+            //                                .padding(.trailing, 16)
+            //                                .padding(.bottom, 16)
+            //                        }
+            //                    }
+            //                }
+            //            }
         }
         .environment(\.layoutDirection, .rightToLeft)
         .navigationBarTitle("beRadio", displayMode: .inline)
@@ -258,7 +264,7 @@ struct ContentView: View {
             newItem.name = station.name
             newItem.url = station.url
             newItem.favicon = station.favicon
-
+            
             do {
                 try viewContext.save()
             } catch {
